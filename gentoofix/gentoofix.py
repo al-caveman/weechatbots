8fa81661627136ba9731713b194da5919636350b
caveman <toraboracaveman@gmail.com>
2018-05-18 22:22:04 +0400

    added capacity for handling corvus`'s haram use of /topic

--- gentoofix/gentoofix.py
+++ gentoofix/gentoofix.py
@@ -11,9 +11,9 @@ import time
 # constant wannabies #
 #                    #
 ######################
-CHANNELS_ACTIVE = [
-    'freenode/#gentoo-chat-exile',
-    #'freenode/##happylol',
+BUFFERS_ACTIVE = [
+    'irc.freenode.#gentoo-chat-exile',
+    #'irc.freenode.##happylol',
 ]
 SILENCE_LIMIT = 50
 TRUTH = [
@@ -48,6 +48,7 @@ BYPASS_NICKS = [
 #    'blap_',
 ]
 BYPASS_PAUSE = 1
+TOPIC_DELAY = .5 # seconds
 
 
 ########################
@@ -68,6 +69,12 @@ random.seed(time.time())
 random.shuffle(TRUTH)
 truth_i = 0
 previous_chat_messages = {}
+iscorvus = {
+    'corvus`' : True,
+    'cavemansmom': True,
+}
+last_approved_topic = ''
+todos = {}
 
 
 ########################
@@ -78,6 +85,14 @@ previous_chat_messages = {}
 def sendresponse(network, channel, response):
     wc.command('', '/msg -server {} {} {}'.format(
         network, channel, response
+    ))
+
+def settopic(buff_id, channel, topic):
+    buff_ptr = wc.buffer_search('==', buff_id)
+    if topic == '':
+        topic = '-delete'
+    wc.command(buff_ptr, '/topic {} {}'.format(
+        channel, topic
     ))
 
 def educate(data, signal, signal_data):
@@ -95,25 +110,74 @@ def educate(data, signal, signal_data):
     msg = m_signal_data.groupdict()['msg']
     msg = msg.lower()
 
-    # channel i.d.
-    chan_id = '{}/{}'.format(network,channel)
+    # buffer i.d.
+    buff_id = 'irc.{}.{}'.format(network,channel)
 
-    # run only in supported channels
-    if chan_id in CHANNELS_ACTIVE:
+    # run only in supported buffers
+    if buff_id in BUFFERS_ACTIVE:
         # send truth only if enough shit was said
-        if chan_id in previous_chat_messages:
-            previous_chat_messages[chan_id] += 1
+        if buff_id in previous_chat_messages:
+            previous_chat_messages[buff_id] += 1
         else:
-            previous_chat_messages[chan_id] = 1
+            previous_chat_messages[buff_id] = 1
 
         # reset counter if some bypass dude talks
         if nickname in BYPASS_NICKS:
-            if previous_chat_messages[chan_id] > BYPASS_PAUSE:
-                previous_chat_messages[chan_id] -= BYPASS_PAUSE
+            if previous_chat_messages[buff_id] > BYPASS_PAUSE:
+                previous_chat_messages[buff_id] -= BYPASS_PAUSE
 
-        if previous_chat_messages[chan_id] % SILENCE_LIMIT == 0:
+        if previous_chat_messages[buff_id] % SILENCE_LIMIT == 0:
             sendresponse(network, channel, TRUTH[truth_i % len(TRUTH)])
             truth_i += 1
+
+    return wc.WEECHAT_RC_OK
+
+def topic(data, signal, signal_data):
+    global iscorvus
+    global last_approved_topic
+    global todos
+
+    # parse input messages
+    network, event = signal.split(',')
+    m_signal_data = re.match(
+        r'^:(?P<nickname>\S+)!(?P<username>\S+)@(?P<host>\S+) \S+ (?P<channel>\S+) :(?P<topic>.*)$',
+        signal_data
+    )
+    nickname = m_signal_data.groupdict()['nickname']
+    username = m_signal_data.groupdict()['username']
+    host     = m_signal_data.groupdict()['host']
+    channel  = m_signal_data.groupdict()['channel']
+    topic    = m_signal_data.groupdict()['topic']
+    nickname = nickname.lower()
+    username = username.lower()
+    host     = host.lower()
+    channel  = channel.lower()
+
+    # buffer i.d.
+    buff_id = 'irc.{}.{}'.format(network,channel)
+
+    # act only in supported buffers
+    if buff_id in BUFFERS_ACTIVE:
+        # extend definitions of corvus
+        if (
+            nickname in iscorvus
+            or username in iscorvus
+            or host in iscorvus
+        ):
+            iscorvus[nickname] = True
+            iscorvus[username] = True
+            iscorvus[host]     = True
+
+        # undo topic only if corvus changed it
+        if nickname in iscorvus:
+            todos['topic'] = (buff_id, channel, 'plz be nice | !OP | SOS')
+        else:
+            if topic.lower().find('caveman') == -1:
+                last_approved_topic = topic
+                todos['topic'] = None
+            else:
+                todos['topic'] = (buff_id, channel, last_approved_topic)
+
 
     return wc.WEECHAT_RC_OK
 
@@ -130,12 +194,24 @@ def think(data, signal, signal_data):
     msg = m_signal_data.groupdict()['msg']
     msg = msg.lower()
 
-    # channel i.d.
-    chan_id = '{}/{}'.format(network,channel)
+    # buffer i.d.
+    buff_id = 'irc.{}.{}'.format(network,channel)
 
-    # run only in supported channels
-    if chan_id in CHANNELS_ACTIVE:
-        previous_chat_messages[chan_id] = 0
+    # run only in supported buffers
+    if buff_id in BUFFERS_ACTIVE:
+        previous_chat_messages[buff_id] = 0
+
+    return wc.WEECHAT_RC_OK
+
+def housekeeping(data, remaining_calls):
+    global todos
+
+    for job in todos:
+        if job == 'topic':
+            if todos[job] is not None:
+                buff_id, channel, topic_clean = todos[job]
+                settopic(buff_id, channel, topic_clean)
+                todos[job] = None
 
     return wc.WEECHAT_RC_OK
 
@@ -146,4 +222,6 @@ def think(data, signal, signal_data):
 #                      #
 ########################
 wc.hook_signal('*,irc_in2_PRIVMSG', 'educate', '')
-wc.hook_signal('*,irc_out_PRIVMSG', 'think', '')
+wc.hook_signal('*,irc_in_TOPIC', 'topic', '')
+#wc.hook_signal('*,irc_out_PRIVMSG', 'think', '')
+#wc.hook_timer(int(TOPIC_DELAY * 1000), 0, 0, "housekeeping", "")
